cfb1a3486ad5ffcb9729b9f5141162278c3c8e21
Henrique Pereira <62767337+halvespereira@users.noreply.github.com>
2020-05-12 14:43:10 -0500

    Pomodoro Clock

--- src/App.js
+++ src/App.js
@@ -1,26 +1,167 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const audio = document.getElementById('beep')
+class App extends React.Component {
+  state = {
+    breakCount: 5,
+    sessionCount: 25,
+    clockCount: 25 * 60,
+    currentTimer: 'Session',
+    isPlaying: false,
+  };
+
+  constructor(props) {
+    super (props);
+    this.loop = undefined;
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.loop);
+  }
+
+  handlePlayPause = () => {
+    const { isPlaying } = this.state;
+
+    if(isPlaying) {
+      clearInterval(this.loop);
+      this.setState({
+        isPlaying: false,
+      });
+    }else {
+      this.setState({
+        isPlaying: true,
+      });
+      this.loop = setInterval(() => {
+        const { clockCount,
+          currentTimer,
+          breakCount,
+          sessionCount } = this.state;
+
+        if (clockCount === 0) {
+          this.setState({
+            currentTimer: (currentTimer === 'Session') ? 'Break' : 'Session',
+            clockCount: (currentTimer === 'Session') ? (breakCount * 60) : (sessionCount * 60),
+          });
+          audio.play();
+        }else {
+          this.setState({
+            clockCount: clockCount - 1
+          })
+        }        
+      }, 1000);
+
+    }
+  }
+
+  handleReset = () => {
+    this.setState({
+      breakCount: 5,
+    sessionCount: 25,
+    clockCount: 25 * 60,
+    currentTimer: 'Session',
+    isPlaying: false,
+    });
+
+    clearInterval(this.loop);
+
+    audio.pause();
+    audio.currentTime = 0;
+  }
+
+  convertToTime = (count) => {
+    let minutes = Math.floor(count/60);
+    let seconds = count % 60;
+    
+    seconds = seconds < 10 ? ('0' + seconds) : seconds;
+    minutes = minutes < 10 ? ('0' + minutes) : minutes;
+
+    return `${minutes}:${seconds}`;
+  };
+
+  handleLengthChange = (count, timerType) => {
+    const  {breakCount, isPlaying, sessionCount, currentTimer} = this.state;
+
+    // eslint-disable-next-line no-unused-vars
+    let newCount;
+
+    if(timerType === 'session'){
+      newCount = sessionCount + count;
+    }else {
+      newCount = breakCount + count;
+    }
+
+    if (newCount > 0 && newCount < 61 && !isPlaying){
+        this.setState({
+         [`${timerType}Count`]: newCount,
+        });
+
+        if (currentTimer.toLowerCase() === timerType) {
+          this.setState({
+            clockCount: newCount * 60
+          })
+        }
+    }
+  }
+
+  
+  render () {
+
+    const {breakCount, 
+      sessionCount, 
+      clockCount, 
+      currentTimer,
+      isPlaying } = this.state
+
+    const breakProps = {
+      title: 'Break',
+      count: breakCount,
+      handleDecrease: () => this.handleLengthChange(-1, 'break'),
+      handleIncrease: () => this.handleLengthChange(1, 'break'),
+    }
+
+    const sessionProps = {
+      title: 'Session',
+      count: sessionCount,
+      handleDecrease: () => this.handleLengthChange(-1, 'session'),
+      handleIncrease: () => this.handleLengthChange(1, 'session'),
+    }
+
+    return (
+      <div>
+        <h1 id="title">Pomodoro Clock</h1>
+        <div className="App">
+          <div className='flex'>
+          <SetTimer {...breakProps}/>
+          <SetTimer {...sessionProps}/>
+          </div>
+          <div className='clock-container'>
+            <h1 id='timer-label'>{currentTimer}</h1>
+            <span id='time-left'>{this.convertToTime(clockCount)}</span>
+
+            <div className="flex">
+            <button id='start_stop' onClick={this.handlePlayPause}><i className={`fas fa-${isPlaying ? 'pause' : 'play'}`}></i></button>
+            <button id='reset' onClick={this.handleReset}><i className='fas fa-sync'></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+const SetTimer = (props) => {
+  const id = props.title.toLowerCase();
+  return (
+  <div className='timer-container'>
+    <h2 id={`${id}-label`}>{props.title} Length</h2>
+    <div className='flex action-wrapper'>
+      <button id={`${id}-decrement`} onClick={props.handleDecrease}><i className='fas fa-minus'></i></button>
+      <span id={`${id}-length`} >{props.count}</span>
+      <button  id={`${id}-increment`} onClick={props.handleIncrease}><i className='fas fa-plus'></i></button>
+    </div>
+  </div>
+) }
 
 export default App;
